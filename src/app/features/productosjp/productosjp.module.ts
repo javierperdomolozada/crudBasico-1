b51c0f5bb082f363b8cbc8d8caac019cfd98462b
calupegithub <calupe@gmail.com>
2022-03-17 07:54:48 -0500

    Estructura de modulos para proyecto compartido feature productos

---
+++ src/app/features/productosjp/productosjp.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductosjpRoutingModule } from './productosjp-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductosjpRoutingModule
+  ]
+})
+export class ProductosjpModule { }
---
+++ src/app/features/productosyg/productosyg.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductosygRoutingModule } from './productosyg-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductosygRoutingModule
+  ]
+})
+export class ProductosygModule { }
