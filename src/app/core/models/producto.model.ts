4d68a45a1f827d4d957c983a1bfd6b84906f041a
Javier Perdomo Lozada <javierperdomolozada@yahoo.es>
2022-03-22 09:06:20 -0500

    Producto 20220322

---
+++ src/app/core/models/producto.model.ts
@@ -0,0 +1,7 @@
+export interface Producto {
+    id: number;
+    nombre: string;
+    descripcion: string;
+    valor: string;
+    imagen: string;
+}
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,12 +6,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
 
 const material = [
   MatCardModule,
   MatButtonModule,
   MatToolbarModule,
   MatIconModule,
+  MatTableModule,
 ];
 
 @NgModule({
