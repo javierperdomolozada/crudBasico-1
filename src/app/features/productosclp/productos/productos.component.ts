260631066bbcaef3a8f55904e53e5662b63c5e5f
yesid.sanchezsalcedo.05@gmail.com <yesid.sanchezsalcedo.05@gmail.com>
2022-03-24 07:05:01 -0500

    master

--- src/app/features/productosclp/productos/productos.component.ts
+++ src/app/features/productosclp/productos/productos.component.ts
@@ -18,7 +18,49 @@ const ELEMENT_DATA: Producto[] = [
     imagen:
       'https://larebajavirtual.com/images/productos/sii/F/300x300/dolex_rapida_accion-30282-1615171578.png',
   },
+
+  {
+    id: 3,
+    nombre: 'Sal de frutas LUA',
+    descripcion: 'Caja x 8 Und',
+    valor: '14900',
+    imagen:
+      'https://larebajavirtual.com/images/productos/sii/F/300x300/sal_de_frutas_lua_original-43654-1624477435.png',
+  },
+  {
+    id: 4,
+    nombre: 'Alka-Seltzer',
+    descripcion: 'Caja x 14 Tab',
+    valor: '2300',
+    imagen:
+      'https://larebajavirtual.com/images/productos/sii/F/300x300/oferta_alka_seltzer_precio_especial-123832-1588036321.png',
+  },
+  {
+    id: 5,
+    nombre: 'PILA VARTA ALKALINA 9V',
+    descripcion: 'Caja x 1 Und',
+    valor: '23000',
+    imagen:
+      'https://larebajavirtual.com/images/productos/sii/F/300x300/pila_varta_alkalina_9v_ref_4922-18779-1627844032.png',
+  },
+  {
+    id: 6,
+    nombre: 'DURACELL PILAS ALCALINAS AA',
+    descripcion: 'Caja x 2 Und',
+    valor: '3200',
+    imagen:
+      'https://larebajavirtual.com/images/productos/sii/F/300x300/duracell_pilas_alcalinas_aa_de_larga_duracion-121175-1640703426.png',
+  },
+  {
+    id: 7,
+    nombre: 'BIOCALCIUM PLUS CALCIO',
+    descripcion: 'Caja x 30 Sobres',
+    valor: '46100',
+    imagen:
+      'https://larebajavirtual.com/images/productos/sii/F/300x300/biocalcium_plus_calcio-104564-1608153513.png',
+  },
 ];
+
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
