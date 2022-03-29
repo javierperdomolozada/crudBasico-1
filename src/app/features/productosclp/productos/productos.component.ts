790fd07789fe9e242c0defef55d8f2610f730823
calupegithub <calupe@gmail.com>
2022-03-29 08:28:27 -0500

    Adicionamos sort table

--- src/app/features/productosclp/productos/productos.component.ts
+++ src/app/features/productosclp/productos/productos.component.ts
@@ -1,8 +1,8 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Producto } from 'src/app/core/models/producto.model';
-
 
 @Component({
   selector: 'app-productos',
@@ -74,6 +74,7 @@ export class ProductosComponent implements OnInit, AfterViewInit {
   //dataSource = ELEMENT_DATA;
   dataSource = new MatTableDataSource(this.listProductos);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor() {}
 
@@ -81,7 +82,7 @@ export class ProductosComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
-    //this.dataSource.sort = this.sort;
+    this.dataSource.sort = this.sort;
   }
 
   applyFilter(event: Event) {
